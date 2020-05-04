7ee4fed6f4981ef4a39ce6639fa5359851aff78c
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-04 15:11:57 +0200

    Arrays in typescript

---
+++ features/arrays.ts
@@ -0,0 +1,7 @@
+const carMakers = ["ford", "toyota", "chevy"];
+//const carMakers: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+const carByMake = [["f150"], ["corolla"], ["camaro"]];
+//const carByMake: string[][] = [];
