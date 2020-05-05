fc11f18f426e54c54cc5f6aeeea36ee5cab0323b
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-05 17:19:10 +0200

    Fields with inheritance

--- features/classes.ts
+++ features/classes.ts
@@ -9,16 +9,19 @@ class Vehicle {
 const vehicle = new Vehicle("orange");
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log("vroom");
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log("vroom");
+  }
 
-//   public startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+  public startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
-// const car = new Car();
-// car.startDrivingProcess();
+const car = new Car(4, "red");
+car.startDrivingProcess();
