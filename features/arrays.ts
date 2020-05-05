bd7ffc08ced196229a0cb5741c27109d98c840dd
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-05 10:28:07 +0200

    Multiple types in arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -5,3 +5,8 @@ const dates = [new Date(), new Date()];
 
 const carByMake = [["f150"], ["corolla"], ["camaro"]];
 //const carByMake: string[][] = [];
+
+//Flexible types
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
