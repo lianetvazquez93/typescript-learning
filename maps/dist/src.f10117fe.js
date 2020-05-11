8ce59c1b43ff7ccd0a09b05431e3f32e7f088fb9
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-11 17:23:33 +0200

    Updating interface definitions

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85120,6 +85120,10 @@ function () {
     };
   }
 
+  User.prototype.markerContent = function () {
+    return "User Name: " + this.name;
+  };
+
   return User;
 }();
 
@@ -85150,6 +85154,10 @@ function () {
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+
+  Company.prototype.markerContent = function () {
+    return "\n      <div>\n        <h1>Company Name: " + this.companyName + "</h1>\n        <h3>Catchphrase: " + this.catchPhrase + "</h3>\n      </div>\n    ";
+  };
 
   return Company;
 }();
@@ -85187,7 +85195,7 @@ function () {
     });
     marker.addListener("click", function () {
       var infoWindow = new google.maps.InfoWindow({
-        content: "Hi there"
+        content: mappable.markerContent()
       });
       infoWindow.open(_this.googleMap, marker);
     });
