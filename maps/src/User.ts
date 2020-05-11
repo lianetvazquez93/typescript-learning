be7430e471ddb21e71fc9f98545f3acf3f5ec413
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-11 11:21:56 +0200

    Export statements

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,13 +1,13 @@
 import faker from "faker";
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  construnctor() {
+  constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
