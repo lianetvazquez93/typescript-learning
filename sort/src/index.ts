29d22e632363f8fb556b712cbca4542051a11948
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-12 17:11:12 +0200

    Separating swapping and comparison

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,21 +1,9 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
+const collection = new NumbersCollection([10, 5, 20]);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
+const sorter = new Sorter(collection);
 
-const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
-console.log(sorter.collection);
+console.log(collection.data);
