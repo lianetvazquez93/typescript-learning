c1b490b5793777a76a5b6e51ae0e7435f69accfe
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-15 12:14:23 +0200

    Sorter abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,19 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([10, 5, 20]);
-// const sorter = new Sorter(collection);
-// sorter.sort();
-//console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 5, 20]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-const sorter = new Sorter(charactersCollection);
-
-sorter.sort();
-
-console.log(charactersCollection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
