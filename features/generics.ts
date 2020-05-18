e9255aa8f89ece69cb50e4f9a28579c42144e40c
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-18 16:16:03 +0200

    Function generics

--- features/generics.ts
+++ features/generics.ts
@@ -1,23 +1,42 @@
-class ArrayOfNumbers {
-  constructor(public collection: number[]) {}
+// class ArrayOfNumbers {
+//   constructor(public collection: number[]) {}
 
-  get(index: number): number {
-    return this.collection[index];
-  }
-}
+//   get(index: number): number {
+//     return this.collection[index];
+//   }
+// }
 
-class ArrayOfStrings {
-  constructor(public collection: string[]) {}
+// class ArrayOfStrings {
+//   constructor(public collection: string[]) {}
 
-  get(index: number): string {
-    return this.collection[index];
-  }
-}
+//   get(index: number): string {
+//     return this.collection[index];
+//   }
+// }
 
 class ArrayOfAnything<T> {
   constructor(public collection: T[]) {}
 
   get(index: number): T {
     return this.collection[index];
+  }
+}
+
+//Generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
   }
 }
