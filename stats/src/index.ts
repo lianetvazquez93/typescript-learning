9d73d67e9b1e3f869dad99c0f047a69583f1fed9
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-18 12:15:14 +0200

    A composition based approach  * Implementing an analyzer class  * Building the reporter  * Putting it all together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,14 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
 
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins);
+summary.buildAndPrintReport(matchReader.matches);
