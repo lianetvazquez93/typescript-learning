e6a56a83dc41282a3c75f3d69d029fbf9da02551
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-18 16:35:23 +0200

    Generic constraints

--- features/generics.ts
+++ features/generics.ts
@@ -23,20 +23,46 @@ class ArrayOfAnything<T> {
 }
 
 //Generics with functions
-function printStrings(arr: string[]): void {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
+// function printStrings(arr: string[]): void {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
 
-function printNumbers(arr: number[]): void {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
+// function printNumbers(arr: number[]): void {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
 
 function printAnything<T>(arr: T[]): void {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
+
+//Generic Constraints
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("Iam  a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
