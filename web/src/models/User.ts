72bd4abe20967d2507074548615ae17b7c4d8b36
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-05-25 10:36:42 +0200

    Extracting an Attributes class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -15,13 +15,4 @@ export class User {
 
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) { }
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
